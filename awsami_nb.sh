7b95fdd60633b9629264c062e59fa8d08c7e3d72
Alan Schwartz <schwartzala@my.easternct.edu>
2018-01-01 14:07:43 -0500

    Create awsami_nb.sh
    Script for setting up an AWS AMI instance.

---
+++ awsami_nb.sh
@@ -0,0 +1,30 @@
+# Create a RHEL7 Box in the AWS Console
+# Open up Port 8888 to have access to the Notebook's default port
+# Run as Super User: sudo su
+# Make this script executable: chmod +777 ./awsami_nb.sh
+# Execute the script: ./awsami_nb.sh
+
+# Update yum
+sudo yum update
+
+# Remove any old Docker Installations
+sudo yum remove docker docker-common docker-selinux docker-engine
+
+# Add the Docker Repo to yum
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+# Install Docker Community Edition
+sudo yum install docker
+
+# Check your Docker installation was successful
+docker
+# Above should be a list of possible Docker commands
+
+# Start Docker service
+sudo service docker start
+
+# Get the Tenserflow Notebook Docker Image
+docker pull jupyter/tensorflow-notebook
+
+# Run the Docker image 
+docker run -it --rm -p 8888:8888 jupyter/tensorflow-notebook
